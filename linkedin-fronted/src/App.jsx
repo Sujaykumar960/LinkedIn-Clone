616ff2501167310e27ecf083468745eb47a3a313
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-05-01 21:16:30 +0530

    first commit

---
+++ linkedin-fronted/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-gray-100 w-[100%] box-border'>
+      <Navbar1 />
+    </div>
+  )
+}
+
+export default App
