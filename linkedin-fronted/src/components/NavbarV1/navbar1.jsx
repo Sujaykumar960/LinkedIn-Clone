186ee63c14f027833ad2072996a8f617fd4e7534
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-05-02 23:00:17 +0530

    first commit

--- linkedin-fronted/src/components/NavbarV1/navbar1.jsx
+++ linkedin-fronted/src/components/NavbarV1/navbar1.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Navbar1 = () => {
   return (
-    <div>Navbar1</div>
+    <nav className='w-[100%] bg-gray-100 md:px-[100px] px-[20px] flex justify-between py-4 box-border'>
+      <div className='flex gap-0 items-center cursor-pointer'>
+        <h3 className='text-blue-800 font-bold text-3xl'>LinkedIn</h3>
+      </div>
+    </nav>
   )
 }
 
