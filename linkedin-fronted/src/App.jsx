0870eb50d12aee4c4f0cf06fea080e1c57bc927c
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-05-02 23:23:45 +0530

    first commit

--- linkedin-fronted/src/App.jsx
+++ linkedin-fronted/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Navbar1 from './components/NavbarV1/navbar1'
 import './App.css'
 
 function App() {
--- linkedin-fronted/src/components/NavbarV1/navbar1.jsx
+++ linkedin-fronted/src/components/NavbarV1/navbar1.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import linkedinLogo from '../Image/Linkedin_logo.png'
 
 const Navbar1 = () => {
   return (
     <nav className='w-[100%] bg-gray-100 md:px-[100px] px-[20px] flex justify-between py-4 box-border'>
       <div className='flex gap-0 items-center cursor-pointer'>
         <h3 className='text-blue-800 font-bold text-3xl'>LinkedIn</h3>
+        <img src={linkedinLogo} alt="LinkedIn Logo" className='w-6 h-6' />
       </div>
     </nav>
   )
