2f220df0a4bff76e5d24351c00c575fa880ddffa
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-05-05 00:21:45 +0530

    first commit

--- linkedin-fronted/src/pages/SignUp/signUp.jsx
+++ linkedin-fronted/src/pages/SignUp/signUp.jsx
@@ -25,10 +25,18 @@ const signUp = () => {
                 <div className='w-full hover:bg-blue-900 bg-blue-800 text-white py-3 px-4 rounded-xl text-center text-xl cursor-pointer my-2'>
                     Register
                 </div>
-
             </div>
 
+            <div className='flex items-center gap-2'>
+                <div className='border-b-1 border-gray-500 w-[45%]'></div><div>or</div><div className='border-b-1 border-gray-500 w-[45%]'></div>
+            </div>
+
+            <div>Sign in with Google</div>
+
         </div>
+
+        <div className='mt-4 mb-10 flex gap-1'>Already on LinkedIn? <div className='text-blue-800 cursor-pointer'>Sign in</div></div>
+
     </div>
   )
 }
