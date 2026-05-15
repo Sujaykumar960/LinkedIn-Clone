b8cb096c5fef968c4b0747334c2c29f19153d299
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-05-15 20:38:08 +0530

    first commit

--- linkedin-fronted/src/pages/Messages/messages.jsx
+++ linkedin-fronted/src/pages/Messages/messages.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../../components/Card/card'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Messages = () => {
   return (
@@ -14,7 +15,19 @@ const Messages = () => {
                     </div>
 
                     <div className='border-b-1 border-gray-300 px-5 py-5 font-semibold text-lg'>
-                        <div className='py-1 px-3 cursor-pointer hover:bg-green-900 bg-green-800'></div>
+                        <div className='py-1 px-3 cursor-pointer hover:bg-green-900 bg-green-800 font-semibold flex gap-2 w-fit rounded-2xl text-white'>Focused <ArrowDropDownIcon /></div>
+                    </div>
+
+                    {/* Chat Card */}
+                    <div className='w-full md:flex md:flex'>
+                        <div className='h-[590px] overflow-auto w-full md:w-[40%] border-r-1 border-gray-400'>
+                            {/* For Each chat Conversation */}
+                            <div className='flex items-center w-full cursor-pointer border-b-1 border-gray-300 gap-3 p-4 hover:bg-gray-200' >
+                                <div>
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNfTkosk_XISYGUe8YAUWMrv0kcP5a4YMcVQ&s" alt="avatar" className='w-10 h-10 rounded-full' />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </Card>
             </div>
