52572831b8106f1cd629165861f28f1270f48025
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-05-15 19:33:26 +0530

    first commit

--- linkedin-fronted/src/pages/Messages/messages.jsx
+++ linkedin-fronted/src/pages/Messages/messages.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Card from '../../components/Card/card'
 
 const Messages = () => {
   return (
     <div className='px-5 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-100'>
-        Messages
+        <div className='w-full justify-between flex pt-5'>
+
+            {/* left side */}
+            <div className='w-full md:w-[70%]'>
+                <Card padding={0}>
+                    <div className='border-b-1 border-gray-300 px-5 py-5 font-semibold text-lg'>
+                        Messaging
+                    </div>
+
+                    <div className='border-b-1 border-gray-300 px-5 py-5 font-semibold text-lg'>
+                        <div className='py-1 px-3 cursor-pointer hover:bg-green-900 bg-green-800'></div>
+                    </div>
+                </Card>
+            </div>
+        </div>
     </div>
   )
 }
