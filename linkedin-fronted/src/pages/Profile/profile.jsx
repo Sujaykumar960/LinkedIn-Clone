4cf869bf3a48c16b7c11d92a3ea56f8c33f8d42a
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-06-27 16:10:46 +0530

    Describe your changes here

--- linkedin-fronted/src/pages/Profile/profile.jsx
+++ linkedin-fronted/src/pages/Profile/profile.jsx
@@ -15,8 +15,20 @@ const Profile = () => {
                     <div className='relative w-full h-[200px]'>
 
                       <img className='w-full h-[200px] rounded-tr-lg rounded-tl-lg' src="https://media.istockphoto.com/id/485371557/photo/twilight-at-spirit-island.jpg?s=612x612&w=0&k=20&c=FSGliJ4EKFP70Yjpzso0HfRR4WwflC6GKfl4F3Hj7fk=" alt="" />
-                      <div className='absolute object-cover top-24 left-6 z-10'><img className='rounded-full border-2 border-white cursor-pointer w-35 h-35' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNfTkosk_XISYGUe8YAUWMrv0kcP5a4YMcVQ&s" alt="" /></div>
+                      <div className='absolute object-cover top-24 left-6 z-10'><img className='rounded-full border-2 border-white cursor-pointer w-30 h-30' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNfTkosk_XISYGUe8YAUWMrv0kcP5a4YMcVQ&s" alt="" /></div>
                     </div>
+
+                    <div className='mt-10 relative px-8 py-2'>
+                      <div className='w-full'>
+                        <div className='text-2xl'>User 1</div>
+                        <div className='text-gray-700'>Software Engineer</div>
+                        <div className='text-sm text-gray-500'>Delhi, India</div>
+                        <div className='text-md text-blue-800 w-fit cursor-pointer hover:underline'>2 connections</div>
+
+                        
+                      </div>
+                    </div>
+
                   </div>
 
                 </Card>
