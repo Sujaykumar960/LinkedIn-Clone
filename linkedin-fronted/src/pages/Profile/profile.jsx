96565e8bd49249de1b386493e4c5e3fae60f6751
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-06-28 21:07:47 +0530

    Describe your changes here

--- linkedin-fronted/src/pages/Profile/profile.jsx
+++ linkedin-fronted/src/pages/Profile/profile.jsx
@@ -21,6 +21,7 @@ const Profile = () => {
                     </div>
 
                     <div className='mt-10 relative px-8 py-2'>
+                      <div className='absolute cursor-pointer top-0 right-3 z-20 w-[35px] flex justify-center items-center h-[35px] rounded-full p-3 bg-white'><EditIcon /></div>
                       <div className='w-full'>
                         <div className='text-2xl'>User 1</div>
                         <div className='text-gray-700'>Software Engineer</div>
@@ -48,17 +49,35 @@ const Profile = () => {
                 </Card>
               </div>
 
-              <div className='my-5'>
+              <div className='mt-5'>
                 <Card padding={1}>
                   <div className='flex justify-between items-center'>
                     <div className='text-xl'>About</div>
+                    <div className='cursor-pointer'><EditIcon /></div>
                   </div>
                   <div className='text-gray-700 text-md w-[80%]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum officia iure facere! Eum reprehenderit ad eius quaerat asperiores quo totam et, dolores sit magnam earum aliquid corrupti saepe ab animi?</div>
                 </Card>
               </div>
               
+              <div className='mt-5'>
+                <Card padding={1}>
+                  <div className='flex justify-between items-center'>
+                    <div className='text-xl'>Skills</div>
+                  </div>
+                  <div className='text-gray-700 text-md my-2 w-full flex gap-4 flex-wrap'>
+                    <div className='py-1 px-2 cursor-pointer bg-blue-800 text-white rounded-lg'>React JS</div>
+                    <div className='py-1 px-2 cursor-pointer bg-blue-800 text-white rounded-lg'>Node JS</div>
+                    <div className='py-1 px-2 cursor-pointer bg-blue-800 text-white rounded-lg'>Express JS</div>
+                    <div className='py-1 px-2 cursor-pointer bg-blue-800 text-white rounded-lg'>MongoDB</div>
+
+                  </div>
+                </Card>
+              </div>
             </div>
 
+
+
+             {/* Right Side Add  */}
             <div className='hidden md:flex md:w-[28%]'>
               <div className='sticky top-19 w-full'>
                 <Advertisement />
