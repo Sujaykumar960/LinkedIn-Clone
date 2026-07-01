6f7daf3716a73d07e80eb7e4a753d52e6d7d2473
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-07-02 01:46:40 +0530

    Describe your changes here

--- linkedin-fronted/src/components/AboutModal/aboutModal.jsx
+++ linkedin-fronted/src/components/AboutModal/aboutModal.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 const AboutModal = () => {
   return (
-    <div>
-        
+    <div className='my-8'>
+        <div className='w-full mb-4'>
+            <label>About*</label>
+            <br />
+            <textarea name="" id="" className='p-2 mt-1 w-full border-1 rounded-md' cols={10} rows={3}></textarea>
+        </div>
+        <div className='w-full mb-4'>
+            <label>Skills*(Add by separating commas)</label>
+            <br />
+            <textarea name="" id="" className='p-2 mt-1 w-full border-1 rounded-md' cols={10} rows={3}></textarea>
+        </div>
+        <div className='w-full mb-4'>
+            <label htmlFor="resumeUpload" className='p-2 bg-blue-800 text-white rounded-lg cursor-pointer'>Resume Upload</label>
+            <input type="file" id="resumeUpload" className='hidden' />
+            <div className='my-2'>fweoifjwjfscagi</div>
+        </div>
+
+        <div className='bg-blue-950 text-white w-fit py-1 px-3 cursor-pointer rounded-2xl'>Save</div>
     </div>
   )
 }
--- linkedin-fronted/src/pages/Profile/profile.jsx
+++ linkedin-fronted/src/pages/Profile/profile.jsx
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import clsx from 'clsx';
 import Advertisement from '../../components/Advertisement/advertisement'
 import Card from '../../components/Card/card'
 import EditIcon from '@mui/icons-material/Edit';
@@ -41,37 +42,37 @@ const Profile = () => {
   }
   
   return (
-    <div className='px-5 xl:px-50 py-5 mt-5 flex flex-col gap-5 w-full pt-12 bg-gray-100'>
-        <div className='flex justify-between'>
+    <div className={clsx('px-5', 'xl:px-50', 'py-5', 'mt-5', 'flex', 'flex-col', 'gap-5', 'w-full', 'pt-12', 'bg-gray-100')}>
+        <div className={clsx('flex', 'justify-between')}>
 
             {/* Left Side Main Section */}
-            <div className='w-full md:w-[70%]'>
+            <div className={clsx('w-full', 'md:w-[70%]')}>
               <div>
                 <Card padding={0}>
-                  <div className='w-full h-fit bg-gray-200'>
-                    <div className='relative w-full h-[200px]'>
-                      <div className='absolute cursor-pointer top-3 right-3 z-20 w-[35px] flex justify-center items-center h-[35px] rounded-full p-3 bg-white' onClick={handleOnEditCover}><EditIcon /></div>
-                      <img className='w-full h-[200px] rounded-tr-lg rounded-tl-lg' src="https://media.istockphoto.com/id/485371557/photo/twilight-at-spirit-island.jpg?s=612x612&w=0&k=20&c=FSGliJ4EKFP70Yjpzso0HfRR4WwflC6GKfl4F3Hj7fk=" alt="" />
-                      <div className='absolute object-cover top-24 left-6 z-10' onClick={handleCircularImageOpen}><img className='rounded-full border-2 border-white cursor-pointer w-30 h-30' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNfTkosk_XISYGUe8YAUWMrv0kcP5a4YMcVQ&s" alt="" /></div>
+                  <div className={clsx('w-full', 'h-fit', 'bg-gray-200')}>
+                    <div className={clsx('relative', 'w-full', 'h-[200px]')}>
+                      <div className={clsx('absolute', 'cursor-pointer', 'top-3', 'right-3', 'z-20', 'w-[35px]', 'flex', 'justify-center', 'items-center', 'h-[35px]', 'rounded-full', 'p-3', 'bg-white')} onClick={handleOnEditCover}><EditIcon /></div>
+                      <img className={clsx('w-full', 'h-[200px]', 'rounded-tr-lg', 'rounded-tl-lg')} src="https://media.istockphoto.com/id/485371557/photo/twilight-at-spirit-island.jpg?s=612x612&w=0&k=20&c=FSGliJ4EKFP70Yjpzso0HfRR4WwflC6GKfl4F3Hj7fk=" alt="" />
+                      <div className={clsx('absolute', 'object-cover', 'top-24', 'left-6', 'z-10')} onClick={handleCircularImageOpen}><img className={clsx('rounded-full', 'border-2', 'border-white', 'cursor-pointer', 'w-30', 'h-30')} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNfTkosk_XISYGUe8YAUWMrv0kcP5a4YMcVQ&s" alt="" /></div>
                     </div>
 
-                    <div className='mt-10 relative px-8 py-2'>
-                      <div className='absolute cursor-pointer top-0 right-3 z-20 w-[35px] flex justify-center items-center h-[35px] rounded-full p-3 bg-white' onClick={handleInfoModal}><EditIcon /></div>
+                    <div className={clsx('mt-10', 'relative', 'px-8', 'py-2')}>
+                      <div className={clsx('absolute', 'cursor-pointer', 'top-0', 'right-3', 'z-20', 'w-[35px]', 'flex', 'justify-center', 'items-center', 'h-[35px]', 'rounded-full', 'p-3', 'bg-white')} onClick={handleInfoModal}><EditIcon /></div>
                       <div className='w-full'>
                         <div className='text-2xl'>User 1</div>
                         <div className='text-gray-700'>Software Engineer</div>
-                        <div className='text-sm text-gray-500'>Delhi, India</div>
-                        <div className='text-md text-blue-800 w-fit cursor-pointer hover:underline'>2 connections</div>
+                        <div className={clsx('text-sm', 'text-gray-500')}>Delhi, India</div>
+                        <div className={clsx('text-md', 'text-blue-800', 'w-fit', 'cursor-pointer', 'hover:underline')}>2 connections</div>
 
-                        <div className='md:flex w-full justify-between'>
-                          <div className='my-5 flex gap-5'>
-                            <div className='cursor-pointer p-2 border-1 rounded-lg bg-blue-800 text-white font-semibold'>Open to</div>
-                            <div className='cursor-pointer p-2 border-1 rounded-lg bg-blue-800 text-white font-semibold'>Share</div>
-                            <div className='cursor-pointer p-2 border-1 rounded-lg bg-blue-800 text-white font-semibold'>Logout</div>
+                        <div className={clsx('md:flex', 'w-full', 'justify-between')}>
+                          <div className={clsx('my-5', 'flex', 'gap-5')}>
+                            <div className={clsx('cursor-pointer', 'p-2', 'border-1', 'rounded-lg', 'bg-blue-800', 'text-white', 'font-semibold')}>Open to</div>
+                            <div className={clsx('cursor-pointer', 'p-2', 'border-1', 'rounded-lg', 'bg-blue-800', 'text-white', 'font-semibold')}>Share</div>
+                            <div className={clsx('cursor-pointer', 'p-2', 'border-1', 'rounded-lg', 'bg-blue-800', 'text-white', 'font-semibold')}>Logout</div>
                           </div>
-                          <div className='my-5 flex gap-5'>
-                            <div className='cursor-pointer p-2 border-1 rounded-lg bg-blue-800 text-white font-semibold'>Open to</div>
-                            <div className='cursor-pointer p-2 border-1 rounded-lg bg-blue-800 text-white font-semibold'>Share</div>
+                          <div className={clsx('my-5', 'flex', 'gap-5')}>
+                            <div className={clsx('cursor-pointer', 'p-2', 'border-1', 'rounded-lg', 'bg-blue-800', 'text-white', 'font-semibold')}>Open to</div>
+                            <div className={clsx('cursor-pointer', 'p-2', 'border-1', 'rounded-lg', 'bg-blue-800', 'text-white', 'font-semibold')}>Share</div>
                           </div>
                         </div>
 
@@ -86,24 +87,24 @@ const Profile = () => {
 
               <div className='mt-5'>
                 <Card padding={1}>
-                  <div className='flex justify-between items-center'>
+                  <div className={clsx('flex', 'justify-between', 'items-center')}>
                     <div className='text-xl'>About</div>
                     <div className='cursor-pointer' onClick={handleAboutModal}><EditIcon /></div>
                   </div>
-                  <div className='text-gray-700 text-md w-[80%]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum officia iure facere! Eum reprehenderit ad eius quaerat asperiores quo totam et, dolores sit magnam earum aliquid corrupti saepe ab animi?</div>
+                  <div className={clsx('text-gray-700', 'text-md', 'w-[80%]')}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum officia iure facere! Eum reprehenderit ad eius quaerat asperiores quo totam et, dolores sit magnam earum aliquid corrupti saepe ab animi?</div>
                 </Card>
               </div>
               
               <div className='mt-5'>
                 <Card padding={1}>
-                  <div className='flex justify-between items-center'>
+                  <div className={clsx('flex', 'justify-between', 'items-center')}>
                     <div className='text-xl'>Skills</div>
                   </div>
-                  <div className='text-gray-700 text-md my-2 w-full flex gap-4 flex-wrap'>
-                    <div className='py-1 px-2 cursor-pointer bg-blue-800 text-white rounded-lg'>React JS</div>
-                    <div className='py-1 px-2 cursor-pointer bg-blue-800 text-white rounded-lg'>Node JS</div>
-                    <div className='py-1 px-2 cursor-pointer bg-blue-800 text-white rounded-lg'>Express JS</div>
-                    <div className='py-1 px-2 cursor-pointer bg-blue-800 text-white rounded-lg'>MongoDB</div>
+                  <div className={clsx('text-gray-700', 'text-md', 'my-2', 'w-full', 'flex', 'gap-4', 'flex-wrap')}>
+                    <div className={clsx('py-1', 'px-2', 'cursor-pointer', 'bg-blue-800', 'text-white', 'rounded-lg')}>React JS</div>
+                    <div className={clsx('py-1', 'px-2', 'cursor-pointer', 'bg-blue-800', 'text-white', 'rounded-lg')}>Node JS</div>
+                    <div className={clsx('py-1', 'px-2', 'cursor-pointer', 'bg-blue-800', 'text-white', 'rounded-lg')}>Express JS</div>
+                    <div className={clsx('py-1', 'px-2', 'cursor-pointer', 'bg-blue-800', 'text-white', 'rounded-lg')}>MongoDB</div>
 
                   </div>
                 </Card>
@@ -111,20 +112,20 @@ const Profile = () => {
 
               <div className='mt-5'>
                 <Card padding={1}>
-                  <div className='flex justify-between items-center'>
+                  <div className={clsx('flex', 'justify-between', 'items-center')}>
                     <div className='text-xl'>Activities</div>
                   </div>
-                  <div className='cursor-pointer px-3 py-1 w-fit border-1 rounded-4xl bg-green-800 text-white font-semibold'>Posts</div>
+                  <div className={clsx('cursor-pointer', 'px-3', 'py-1', 'w-fit', 'border-1', 'rounded-4xl', 'bg-green-800', 'text-white', 'font-semibold')}>Posts</div>
 
                   {/* Parent div for scrollable Activities */}
-                  <div className='overflow-x-auto my-2 flex gap-1 overflow-y-hidden w-full'>
-                    <div className='cursor-pointer shrink-0 w-[350px]'>
+                  <div className={clsx('overflow-x-auto', 'my-2', 'flex', 'gap-1', 'overflow-y-hidden', 'w-full')}>
+                    <div className={clsx('cursor-pointer', 'shrink-0', 'w-[350px]')}>
                       <Post profile={1}/>
                     </div>
-                    <div className='cursor-pointer shrink-0 w-[350px]'>
+                    <div className={clsx('cursor-pointer', 'shrink-0', 'w-[350px]')}>
                       <Post profile={1}/>
                     </div>
-                    <div className='cursor-pointer shrink-0 w-[350px]'>
+                    <div className={clsx('cursor-pointer', 'shrink-0', 'w-[350px]')}>
                       <Post profile={1}/>
                     </div>
 
@@ -134,29 +135,29 @@ const Profile = () => {
 
               <div className='mt-5'>
                 <Card padding={1}>
-                  <div className='flex justify-between items-center'>
+                  <div className={clsx('flex', 'justify-between', 'items-center')}>
                     <div className='text-xl'>Experience</div>
                     <div className='cursor-pointer'><AddIcon /></div>
                   </div>
 
                   <div className='mt-5'>
 
-                    <div className='p-2 border-t-1 border-gray-300 flex justify-between'>
+                    <div className={clsx('p-2', 'border-t-1', 'border-gray-300', 'flex', 'justify-between')}>
                       <div>
                         <div className='text-lg'>DSE Engineer | Full stack Engineer</div>
                         <div className='text-sm'>Amazon</div>
-                        <div className='text-sm text-gray-500'>March 2022, Present</div>
-                        <div className='text-sm text-gray-500'>Delhi, India</div>
+                        <div className={clsx('text-sm', 'text-gray-500')}>March 2022, Present</div>
+                        <div className={clsx('text-sm', 'text-gray-500')}>Delhi, India</div>
                       </div>
                       <div className='cursor-pointer'><EditIcon /></div>
                     </div>
                     
-                    <div className='p-2 border-t-1 border-gray-300 flex justify-between'>
+                    <div className={clsx('p-2', 'border-t-1', 'border-gray-300', 'flex', 'justify-between')}>
                       <div>
                         <div className='text-lg'>DSE Engineer | Full stack Engineer</div>
                         <div className='text-sm'>Amazon</div>
-                        <div className='text-sm text-gray-500'>March 2022, Present</div>
-                        <div className='text-sm text-gray-500'>Delhi, India</div>
+                        <div className={clsx('text-sm', 'text-gray-500')}>March 2022, Present</div>
+                        <div className={clsx('text-sm', 'text-gray-500')}>Delhi, India</div>
                       </div>
                       <div className='cursor-pointer'><EditIcon /></div>
                     </div>
@@ -170,8 +171,8 @@ const Profile = () => {
 
 
              {/* Right Side Add  */}
-            <div className='hidden md:flex md:w-[28%]'>
-              <div className='sticky top-19 w-full'>
+            <div className={clsx('hidden', 'md:flex', 'md:w-[28%]')}>
+              <div className={clsx('sticky', 'top-19', 'w-full')}>
                 <Advertisement />
               </div>
 
@@ -193,8 +194,8 @@ const Profile = () => {
         }
 
         {
-          aboutModal && <Modal title="About" closeModal={handleAboutModal}>
-            <AboutModal />
+          aboutModal && <Modal title="Edit About" closeModal={handleAboutModal}>
+            <AboutModal /> 
           </Modal>
         }
     
