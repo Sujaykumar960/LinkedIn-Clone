3a4ee9afc30cf86347916ae5209b08b729fa1514
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-07-04 01:30:07 +0530

    Describe your changes here

--- linkedin-fronted/src/pages/Notification/notification.jsx
+++ linkedin-fronted/src/pages/Notification/notification.jsx
@@ -19,7 +19,27 @@ const Notification = () => {
       <div className='w-full py-5 sm:w-[50%]'>
         <div>
             <Card padding={0}> 
+                <div className='w-full'>
 
+                    {/* For each Notifications */}
+                    <div className={`border-b cursor-pointer flex gap-4 items-center border-gray-300 p-3`}>
+                        <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="" className='rounded-full cursor-pointer w-17 h-17'/>
+                        <div>Dummy User1 has sent you a connection request</div>
+                    </div>
+
+                    {/* For each Notifications */}
+                    <div className={`border-b cursor-pointer flex gap-4 items-center border-gray-300 p-3`}>
+                        <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="" className='rounded-full cursor-pointer w-17 h-17'/>
+                        <div>Dummy User1 has commented on your post</div>
+                    </div>
+
+                    {/* For each Notifications */}
+                    <div className={`border-b cursor-pointer flex gap-4 items-center border-gray-300 p-3`}>
+                        <img src="https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8=" alt="" className='rounded-full cursor-pointer w-17 h-17'/>
+                        <div>Dummy User2 has sent you a connection request</div>
+                    </div>
+
+                </div>
             </Card>
         </div>
       </div>
