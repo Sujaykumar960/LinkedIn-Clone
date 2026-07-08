7b6fcba887a51af4c19216abd9cb1b83a0719a2e
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-07-08 20:15:43 +0530

    Describe your changes here

--- linkedin-backend/controller/user.js
+++ linkedin-backend/controller/user.js
@@ -1,10 +1,21 @@
-
+const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
 
 
 exports.register = async (req, res) => {
     try{
-        console.log(req.body);
-        // let {email, password, f_name} = req.body;
+        let {email, password, f_name} = req.body;
+        let isUserExist = await User.findOne({email:email});
+        if(isUserExist){
+            return res.status(400).json({ error: "Already Have an account with this email .Please try with other email"});
+        }
+        const hashedPassword = await bcryptjs.hash(password,10);
+        console.log(hashedPassword)
+        const newUser = new User({email,password:hashedPassword,f_name});
+        await newUser.save();
+
+        return res.status(201).json({ meaasge: 'User registered successfully', success: "yes", data: newUser});
+
     }catch(err){
         console.error(err);
         res.status(500).json({ error: 'server error',message:err.message });
