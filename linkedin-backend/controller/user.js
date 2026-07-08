0ce2b3a0bbdb79d18c354813ef6a768a21982db4
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-07-08 19:50:56 +0530

    Describe your changes here

--- linkedin-backend/controller/user.js
+++ linkedin-backend/controller/user.js
@@ -3,9 +3,10 @@
 
 exports.register = async (req, res) => {
     try{
-
+        console.log(req.body);
+        // let {email, password, f_name} = req.body;
     }catch(err){
         console.error(err);
-        res.status(500).json({ error: 'server error' });
+        res.status(500).json({ error: 'server error',message:err.message });
     }
 }
