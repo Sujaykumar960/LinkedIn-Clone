8308edec95e26cb564f904cd4bee0f38389f200a
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-07-26 23:32:09 +0530

    Describe your changes here

--- linkedin-backend/controller/conversation.js
+++ linkedin-backend/controller/conversation.js
@@ -7,7 +7,7 @@ const { create } = require('../models/user');
 exports.addConversation = async(req,res) => {
     try{
         let senderId = req.user._id;
-        let { receiverId } = req.body;
+        let { receiverId, message } = req.body;
         let isConvExist = await ConversationModal.findOne({
             members: {
                 $all: [senderId, receiverId]
@@ -19,10 +19,10 @@ exports.addConversation = async(req,res) => {
                 members: [senderId, receiverId]
             });
             await newConversation.save();
-            let addMessage = new MessageModal({ sender: req.user._id, conversation: newConversation._id, message })
+            let addMessage = new MessageModal({ sender: req.user._id, conversation: newConversation._id, message });
             await addMessage.save();
         } else {
-            let addMessage = new MessageModal({ sender: req.user._id, conversation: isConvExist._id, message })
+            let addMessage = new MessageModal({ sender: req.user._id, conversation: isConvExist._id, message });
             await addMessage.save();
         }
 
