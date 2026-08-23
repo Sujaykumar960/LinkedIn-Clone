a5db23c3a647af67b54d68b53d743415a96bc29b
Sujaykumar960 <Sujaykumarixa@gmail.com>
2026-08-23 12:43:35 +0530

    Describe your changes here

--- linkedin-fronted/src/pages/AllActivities/allActivities.jsx
+++ linkedin-fronted/src/pages/AllActivities/allActivities.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProfileCard from '../../components/ProfileCard/profileCard'
 import Advertisement from '../../components/Advertisement/advertisement'
 import { useParams } from 'react-router-dom'
 import Card from '../../components/Card/card'
 import Post from '../../components/Post/post'
+import axios from 'axios'
 
 const AllActivities = () => {
     const { id } = useParams();
 
-    console.log(id);
+    const [post, setPosts] = useState([]);
+    const [ownData, setOwnData] = useState(null);
+
+    const fetchDataOnLoad = async() => {
+      await axios.get(`http://localhost:4000/api/post/getAllPostForUser/${id}`).then(res => {
+        console.log(res);
+        setPosts(res.data.posts);
+      }).catch(err => {
+        console.log(err);
+        alert(err?.response?.data?.error);
+      })
+    }
+
+    useEffect(() => {
+      fetchDataOnLoad();
+    }, [id])
+
   return (
     <div className='px-5 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-100'>
       {/* left side bar */}
       <div className='w-[21%] sm:block sm:w-[23%] hidden py-5'>
         <div className='h-fit'>
-          <ProfileCard />
+          <ProfileCard data={post[0]?.user} />
         </div>
 
         
@@ -31,13 +48,15 @@ const AllActivities = () => {
 
                 <div className='my-2 flex flex-col gap-2'>
                     
-                    <div>
-                        <Post />
-                    </div>
-                    
-                    <div>
-                        <Post />
-                    </div>
+                    {
+                      post.map((item, index) => {
+                        return(
+                         <div key={index}>
+                           <Post item={item} personalData={post[0]?.user} />
+                          </div>
+                        )
+                      })
+                    }
 
                 </div>
             </Card>
